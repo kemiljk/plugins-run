f5a274a1219ec71578a4f35f3f1635108b3283d8
Karl Koch <hi@kejk.co>
2021-03-09 09:01:55 +0000

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex mx-auto md:mx-8 py-4 justify-between items-center">
+    <Logo />
+    <div class="flex max-w-3xl justify-center items-center">
+      <h1 class="text-5xl font-black text-gray-800">
+        plugins<span class="text-gray-400">.</span>run
+      </h1>
+    </div>
+    <Spacer />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
