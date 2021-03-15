5d4ec5c6debbd1329ba2e2d993a87ab9a9e08710
Karl Koch <hi@kejk.co>
2021-03-15 09:16:34 +0000

    feat: updated to include coming soon and favicon

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="flex mx-auto md:mx-8 py-4 justify-between items-center">
-    <Logo />
-    <div class="flex max-w-3xl justify-center items-center">
-      <h1 class="text-5xl font-black text-gray-800">
-        plugins<span class="text-gray-400">.</span>run
+  <div>
+    <nav class="flex mx-auto md:mx-8 py-4 justify-between items-center">
+      <Logo />
+      <div class="flex max-w-3xl justify-center items-center">
+        <h1 class="text-5xl font-black text-gray-800">
+          plugins<span class="text-gray-400">.</span>run
+        </h1>
+      </div>
+      <Spacer />
+    </nav>
+    <div class="grid h-screen mx-auto max-w-3xl place-items-center">
+      <h1 class="text-center font-extrabold text-3xl text-gray-400">
+        Coming soon
       </h1>
     </div>
-    <Spacer />
   </div>
 </template>
 
