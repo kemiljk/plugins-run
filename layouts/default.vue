bbfc3d32f22a14552a2654399ab7e8ed8a617bd2
Karl Koch <karl@kejk.tech>
2021-03-29 10:09:47 +0100

    feat: dedication to my father

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="pb-16 h-full w-full">
-    <Nuxt />
+    <div>
+      <Nuxt />
+    </div>
+    <div class="w-full flex justify-center items-center pt-16">
+      <p
+        class="w-1/3 text-center p-4 bg-gray-100 dark:bg-gray-800 rounded-xl text-black dark:text-white"
+      >
+        This site, and all of my work, is dedicated to my hero, my inspiration,
+        and the strongest man I've ever known. My father. I love you Dad ❤️.
+      </p>
+    </div>
   </div>
 </template>
 
