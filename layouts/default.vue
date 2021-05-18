bb5afc377b69792ae63b0f5cebe3bd0dca6d3a32
Karl <karl@kejk.tech>
2021-05-18 07:45:45 +0100

    fix: tweaks to footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,17 @@
     </div>
     <div class="w-full flex justify-center items-center pt-16 px-4">
       <p
-        class="w-full md:w-1/3 text-center p-4 bg-gray-100 dark:bg-gray-800 rounded-xl text-black dark:text-white"
+        class="w-full md:2/3 lg:w-1/3 text-center p-4 bg-gray-100 dark:bg-gray-800 rounded-xl text-black dark:text-white"
       >
         This site, and all of my work, is dedicated to my hero, my inspiration,
         and the strongest man I've ever known. My father. I love you Dad ❤️.
+      </p>
+    </div>
+    <div class="w-full flex justify-center items-center pt-16 px-4">
+      <p
+        class="w-full md:2/3 lg:w-1/3 text-center text-black dark:text-white"
+      >
+        All plugins and this site are built and maintained by <a href="https://www.kejk.tech">Karl Emil James Koch<\a>.
       </p>
     </div>
   </div>
