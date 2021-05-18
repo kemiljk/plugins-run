16feed8ba6eca0bb344c92fec27e736c65fef2f7
Karl <karl@kejk.tech>
2021-05-18 07:52:20 +0100

    fix: footer layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="w-full flex justify-center items-center pt-16 px-4">
       <p
-        class="w-full md:2/3 lg:w-1/3 text-center p-4 bg-gray-100 dark:bg-gray-800 rounded-xl text-black dark:text-white"
+        class="w-full md:w-2/3 lg:w-1/3 text-center p-4 bg-gray-100 dark:bg-gray-800 rounded-xl text-black dark:text-white"
       >
         This site, and all of my work, is dedicated to my hero, my inspiration,
         and the strongest man I've ever known. My father. I love you Dad ❤️.
@@ -13,7 +13,7 @@
     </div>
     <div class="w-full flex justify-center items-center pt-16 px-4">
       <p
-        class="w-full md:w-2/3 lg:w-1/3 text-center text-black dark:text-white"
+        class="w-full lg:w-1/3 text-center text-black dark:text-white"
       >
         All plugins and this site are built and maintained by <a href="https://www.kejk.tech" class="underline font-bold" target="_blank">Karl Emil James Koch</a>.
       </p>
