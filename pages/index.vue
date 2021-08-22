b1bddeaca064778adaa0f99f345ceceadeb9fcf6
Karl <karl@kejk.tech>
2021-08-22 20:39:19 +0100

    >

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@
 
 <script>
 import getSiteMeta from "../utils/getSiteMeta";
-// import fetchFigmaData from "../scripts/fetch";
+import fetchFigmaData from "../scripts/fetch";
 
 const Cosmic = require("cosmicjs");
 const api = Cosmic();
@@ -80,89 +80,8 @@ export default {
   },
   methods: {
     async fetchFigmaData() {
-    const PLUGINS = [
-    {
-      "id": "1005464966294664447",
-      "name": "batch-styler"
-    },
-    {
-      "id": "961254533460516019",
-      "name": "batch-image"
-    },
-    {
-      "id": "984840399144699277",
-      "name": "better-logos"
-    },
-    {
-      "id": "939597261011175499",
-      "name": "default-export"
-    },
-    {
-      "id": "963807322482539548",
-      "name": "get-instances"
-    },
-    {
-      "id": "837070613195594890",
-      "name": "handover"
-    },
-    {
-      "id": "944991908887656950",
-      "name": "perfect-radius"
-    },
-    {
-      "id": "936748515482279151",
-      "name": "placeholder"
-    },
-    {
-      "id": "965889760815013524",
-      "name": "replace-all"
-    },
-    {
-      "id": "939429933336921092",
-      "name": "round-all"
-    },
-    {
-      "id": "962438678016435824",
-      "name": "scale-better"
-    },
-    {
-      "id": "952557548511056612",
-      "name": "shape-to-frame"
-    },
-    {
-      "id": "977968775104001765",
-      "name": "space-cleaner"
-    }
-  ]
-
-function fetchFigmaData() {
-  PLUGINS.forEach((plugin) => {
-  fetch("../api/plugin-stats.js")
-    .then((response) => response.json())
-    .then((data) => {
-      document
-        .getElementById(plugin.name + "-install-count")
-        if (plugin.name + "-install-count" === made.metadata.installCount) {
-      document  
-        .prepend(kFormatter(data.install_count))
-        };
-      document
-        .getElementById(plugin.name + "-like-count")
-        if (plugin.name + "-like-count" === made.metadata.likeCount) {
-      document 
-        .prepend(kFormatter(data.like_count))
-        };
-    });
-  });
-}
       const data = fetchFigmaData;
       this.fetchFigmaData = data;
-      
-      function kFormatter(num) {
-  return Math.abs(num) > 999
-    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
-    : Math.sign(num) * Math.abs(num);
-}
     },
     async getMadesData() {
       this.loading = true;
