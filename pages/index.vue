373c3a55f972a1652e6b5d0a4b20e8b9d73e6ca7
Karl <karl@kejk.tech>
2021-08-22 20:35:50 +0100

    fixes

--- pages/index.vue
+++ pages/index.vue
@@ -154,8 +154,15 @@ function fetchFigmaData() {
         };
     });
   });
+}
       const data = fetchFigmaData;
       this.fetchFigmaData = data;
+      
+      function kFormatter(num) {
+  return Math.abs(num) > 999
+    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
+    : Math.sign(num) * Math.abs(num);
+}
     },
     async getMadesData() {
       this.loading = true;
